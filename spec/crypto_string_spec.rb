a9f56afc1449ba216bef3113c843a31d053204a9
Greg Conen <gregconen@gmail.com>
2015-08-09 12:02:19 -0700

    add tests for creating byte arrays

--- spec/crypto_string_spec.rb
+++ spec/crypto_string_spec.rb
@@ -2,11 +2,31 @@ require 'rspec'
 require 'crypto_string'
 
 describe CryptoString do
-  
+  describe "creates a byte array" do
+    it "from plaintext" do
+      str = CryptoString.from_plaintext("Lorem")
+      expect str.bytes.to eq([76, 111, 114, 101, 109])
+    end
+
+    it "from hex-encoded strings" do
+      str = CryptoString.from_hex("4C6F72656D")
+      expect str.bytes.to eq([76, 111, 114, 101, 109])
+    end
+
+    it "from base64-encoded strings" do
+      str = CryptoString.from_base64("TG9yZW0=")
+      expect str.bytes.to eq([76, 111, 114, 101, 109])
+    end
+  end
 
   it "converts from hex-encoded strings to base64 encoded strings" do
     str = CryptoString.from_hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
     expect str.base64.to eq("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+  end
+
+  it "converts from base64 strings to hex strings" do
+    str = CryptoString.from_hex("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+    expect str.base64.to eq("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
   end
 
 end
