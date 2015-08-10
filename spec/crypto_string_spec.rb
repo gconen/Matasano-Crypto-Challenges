4f1018b6d30cc05774031e7aa2b5c18182df3be6
Greg Conen <gregconen@gmail.com>
2015-08-10 07:15:37 -0700

    add structure to tests

--- spec/crypto_string_spec.rb
+++ spec/crypto_string_spec.rb
@@ -1,7 +1,7 @@
 require 'crypto_string'
 
 describe CryptoString do
-  describe "creates a byte array" do
+  context "creates a byte array" do
     it "from plaintext" do
       str = CryptoString.from_plaintext("Lorem")
       expect(str.bytes).to eq([76, 111, 114, 101, 109])
@@ -18,7 +18,7 @@ describe CryptoString do
     end
   end
 
-  context "recover hex-encoded string" do
+  context "recovers hex-encoded string" do
     it "recovers the same hex-encoded string" do
       str = CryptoString.from_hex("4C6F72656D")
       expect(str.hex.upcase).to eq("4C6F72656D")
@@ -35,7 +35,7 @@ describe CryptoString do
     end
   end
 
-  context "recover plaintext" do
+  context "recovers plaintext" do
     it "recovers the same plaintext string" do
       str = CryptoString.from_plaintext("Lorem")
       expect(str.plaintext).to eq("Lorem")
@@ -47,7 +47,7 @@ describe CryptoString do
     end
   end
 
-  context "recover base64" do
+  context "recovers base64" do
     it "converts hex-encoded strings to base64-encoded strings" do
       str = CryptoString.from_hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
       expect(str.base64).to eq("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
