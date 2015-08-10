4e6b59156699c03d659d1911a477c8ec18c25ffb
Greg Conen <gregconen@gmail.com>
2015-08-10 05:38:04 -0700

    add creating and setting from plaintext

--- lib/crypto_string.rb
+++ lib/crypto_string.rb
@@ -1,19 +1,42 @@
 class CryptoString
+  attr_reader :bytes, :plaintext, :hex_string, :base64_string
+
   def initialize
-    raise "not implemented"
   end
 
   def self.from_base64(base64_string)
-    @base64_string = base64_string
+    str.base64_string = base64_string
+
+    str
   end
 
   def self.from_hex(hex_string)
-    @hex_string = hex_string
+    str.hex_string = hex_string
+
+    str
   end
 
   def self.from_plaintext(plaintext_string)
     raise "non-ascii characters" unless plaintext_string.ascii_only?
-    @plaintext_string = plaintext_string
-    @bytes = plaintext_string.bytes
+    str = CryptoString.new
+    str.set_plaintext(plaintext_string)
   end
+
+  def set_plaintext(plaintext_string)
+    raise "non-ascii characters" unless plaintext_string.ascii_only?
+    
+    clear
+    @plaintext = plaintext_string
+    @bytes = plaintext_string.bytes
+
+    self
+  end
+
+  private
+  def clear
+    @plaintext = nil
+    @hex_string = nil
+    @base64_string = nil
+  end
+
 end
