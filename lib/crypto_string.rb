8e17f59b3dfadc7123fe966145aae173ca2b4a56
Greg Conen <gregconen@gmail.com>
2015-08-11 18:34:22 -0700

    add base64

--- lib/crypto_string.rb
+++ lib/crypto_string.rb
@@ -1,3 +1,5 @@
+require "base64"
+
 class CryptoString
 
   attr_reader :bytes
@@ -6,9 +8,8 @@ class CryptoString
   end
 
   def self.from_base64(base64_string)
-    str.base64_string = base64_string
-
-    str
+    str = CryptoString.new
+    str.set_base64(base64_string)
   end
 
   def self.from_hex(hex_string)
@@ -22,15 +23,29 @@ class CryptoString
     str.set_plaintext(plaintext_string)
   end
 
+  def base64
+    return @base64_string if @base64_string
+
+    @base64_string = Base64.strict_encode64(plaintext)
+  end
+
   def hex
     return @hex_string if @hex_string
 
-    @bytes.map{ |byte| byte.to_s(16) }.join("")
+    @hex_string = @bytes.map{ |byte| byte.to_s(16) }.join("")
   end
 
   def plaintext
     return @plaintext if @plaintext
-    @bytes.pack("C*")
+    @plaintext = @bytes.pack("C*")
+  end
+
+  def set_base64(base64_string)
+    #clear not needed because it's in set_plaintext
+    set_plaintext(Base64.strict_decode64(base64_string))
+    @base64_string = base64_string
+
+    self
   end
 
   def set_hex(hex_string)
