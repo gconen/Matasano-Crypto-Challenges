f1579b453b415f937170e7eb2348918dad968dd6
Greg Conen <gregconen@gmail.com>
2015-08-11 18:35:53 -0700

    add another test

--- spec/crypto_string_spec.rb
+++ spec/crypto_string_spec.rb
@@ -48,6 +48,11 @@ describe CryptoString do
   end
 
   context "recovers base64" do
+    it "recovers the same base64 string" do
+      str = CryptoString.from_base64("TG9yZW0=")
+      expect(str.base64).to eq("TG9yZW0=")
+    end
+
     it "converts hex-encoded strings to base64-encoded strings" do
       str = CryptoString.from_hex("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
       expect(str.base64).to eq("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
