e332ccc4b3943cd9c2f7b0a566da784fdd2c866c
Greg Conen <gregconen@gmail.com>
2015-08-18 15:45:29 -0700

    improve code clarity

--- lib/crypto_string.rb
+++ lib/crypto_string.rb
@@ -86,12 +86,12 @@ class CryptoString
   end
 
   def english_string_score
-    ascii_values_of_common_letters = #also space
+    ascii_values_of_common_characters = #etainshrdlu and space
           [101, 116, 105, 97, 110, 115, 104, 100, 108, 117, 32]
     score = 0
     bytes.each do |byte|
       return -1 unless byte.between?(32, 126) #printable characters
-      score += 1 if ascii_values_of_common_letters.include?(byte)
+      score += 1 if ascii_values_of_common_characters.include?(byte)
     end
 
     score
